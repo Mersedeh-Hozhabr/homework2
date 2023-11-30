407a0f6d1afc35252d824751dc60ed184a12a973
Mersedeh-Hozhabr <152508489+Mersedeh-Hozhabr@users.noreply.github.com>
2023-11-30 17:53:23 +0330

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,11 @@
+op = input('Enter op(+, -, *, /)')
+a = int(input('Enter a:'))
+b = int(input('Enter b:'))
+if op == "+":
+    print(a+b)
+if op == "_":
+    print(a-b)
+if op == "*":
+    print(a*b)
+if op == "/":
+    print(a/b)
